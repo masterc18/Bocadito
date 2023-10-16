8eebc7e36c89db0152617d33a4624857037c2b08
masterc18 <carlos.talavera16038@est.uca.edu.ni>
2023-10-16 14:22:47 -0600

    Se mejoro el sistema de creacion de cuena

--- singup/SingUp.js
+++ singup/SingUp.js
@@ -4,3 +4,10 @@ const show = document.getElementById("show1");
 show.onchange = function (e) {
   password.type = show.checked ? "text" : "password";
 };
+
+const password2 = document.getElementById("password2");
+const show2 = document.getElementById("show2");
+
+show2.onchange = function (e) {
+  password2.type = show2.checked ? "text" : "password";
+};
