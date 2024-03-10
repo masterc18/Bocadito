0b589897ab234f67d391406f03393a8b20bce9b0
Carlos Talavera <carlos.talavera16038@est.uca.edu.ni>
2024-03-09 20:57:17 -0600

    Se eliminaron los eventos del index

--- app/src/js/login.js
+++ app/src/js/login.js
@@ -1,8 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   const passwordInput = document.getElementById("password");
   const showPassword = document.getElementById("show");
-  const bread = document.querySelector("#Bread");
-  const phrase = document.querySelector("#card");
   showPassword.addEventListener("change", function () {
     if (this.checked) {
       passwordInput.type = "text";
@@ -10,11 +8,5 @@ document.addEventListener("DOMContentLoaded", function () {
       passwordInput.type = "password";
     }
   });
-  bread.addEventListener("click", () => {
-    phrase.classList.add("show");
-  });
-
-  bread.addEventListener("click", () => {
-    phrase.classList.remove("show");
-  });
+  
 });
