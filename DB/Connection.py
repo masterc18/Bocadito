350d25d70347197e4b8c6e041264f551a5f1e9a3
Carlos Talavera <carlos.talavera16038@est.uca.edu.ni>
2024-03-14 22:50:17 -0600

    Se crearon los metodos de conexion y desconexion de la base de datos

--- DB/Connection.py
+++ DB/Connection.py
@@ -1,14 +1,28 @@
 import mysql.connector
+import os
 
-
+# Se crea la clase conexion con los atributos
 class Connection:
     def __init__(self, user, password, database, host):
         self.user = user
-        self. password = password
-        self. database = database
-        self. host = host
+        self.password = password
+        self.database = database
+        self.host = host
         self.conn = None
 
-    def GetConnect():
+    #Se crea el metodo de obtener la conexion con la base de datos
+    def GetConnect(self):
         if (self.conn == None):
-            self. conn == mysql.connector.connect()
+            self. conn == mysql.connector.connect(
+                user=self.user,
+                password=self.password,
+                host=self.host,
+                database=self.database
+            )
+            return self.conn
+
+    # se crea el metodo para desconectar la base datos
+    def GetDisconnect(self):
+        if (self.conn != None):
+            self.conn.close()
+            self.conn = None
