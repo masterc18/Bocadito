59711d21459e999304c8e2ae969719c271c00cf4
Carlos Talavera <carlos.talavera16038@est.uca.edu.ni>
2024-03-16 16:16:57 -0600

    Se mejoro la conectividad de la base de datos

--- DB/Connection.py
+++ DB/Connection.py
@@ -1,39 +1,11 @@
 import mysql.connector
 import os
 
-# Se crea la clase conexion con los atributos
-
-
-class Connection:
-    def __init__(self, user, password, database, host):
-        self.user = user
-        self.password = password
-        self.database = database
-        self.host = host
-        self.conn = None
-
-    # Se crea el metodo de obtener la conexion con la base de datos
-    def GetConnect(self):
-        if (self.conn == None):
-            self. conn == mysql.connector.connect(
-                user=self.user,
-                password=self.password,
-                host=self.host,
-                database=self.database
-            )
-            return self.conn
-
-    # se crea el metodo para desconectar la base datos
-    def GetDisconnect(self):
-        if (self.conn != None):
-            self.conn.close()
-            self.conn = None
-
-
-x = Connection("localhost", "root", "carlosftg1923", "Bocadito")
-conn = x.GetConnect()
-
-if conn != None:
-    print("Conexion exitosa")
-else:
-    print("Error de conexion")
+conexion = mysql.connector.connect(
+    user="root",
+    password="carlosftg1923",
+    host="localhost",
+    database = "Bocadito",
+    port = "3306"
+)
+print(conexion)
