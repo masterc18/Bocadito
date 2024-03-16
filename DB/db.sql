b624888c30efa06186ceb60d27ac4a9056a143d6
Carlos Talavera <carlos.talavera16038@est.uca.edu.ni>
2024-03-16 16:29:26 -0600

    Se elimino el archivo anterior y se reemplazo con este

--- DB/db.sql
+++ DB/db.sql
@@ -1,21 +1,6 @@
--- Se creo la base de datos
+-- Se crea la base de datos
 create database Bocadito;
--- Muestra las bases de datos
-show databases;
--- Se especifica que base de datos se usa
+-- Decimos que DB vamos a usar
 use Bocadito;
--- Se creo la tabla clientes
-CREATE table cliente(
-	id int not null auto_increment,
-    nombre varchar(50),
-    apellido varchar(50),
-    direccion varchar(200),
-    telefono int,
-    -- La llave primaria es el id
-    primary key (id)
-);
--- Se inserto el primer registro
-INSERT INTO cliente (nombre, apellido, direccion, telefono) VALUES ('Carlos', 'Talavera', 'Nandsime', 57968298);
--- Muestra la tabla
-SELECT * FROM cliente;
--- DROP DATABASE nombre de la BD (este comando elimina una base de datos)
+-- Mostramos la base de datos
+show databases;
