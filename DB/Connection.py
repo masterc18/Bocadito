896e9823da9ebb6807e5557adb6439554e9e0272
Carlos Talavera <carlos.talavera16038@est.uca.edu.ni>
2024-03-16 16:51:42 -0600

    Se agrego el metodo de conexion por POO

--- DB/Connection.py
+++ DB/Connection.py
@@ -1,11 +1,47 @@
 import mysql.connector
 import os
+# forma 1
 
-conexion = mysql.connector.connect(
-    user="root",
-    password="carlosftg1923",
-    host="localhost",
-    database = "Bocadito",
-    port = "3306"
-)
-print(conexion)
+
+class Connection:
+    def __init__(self, host, database, port, user, password):
+        self.host = host
+        self.database = database
+        self.port = port
+        self.user = user
+        self.password = password
+        self.conn = None
+
+    def getConnect(self):
+        if self.conn == None:
+            self.conn = mysql.connector.connect(
+                host=self.host,
+                database=self.database,
+                port=self.port,
+                user=self.user,
+                password=self.password
+            )
+            return self.conn
+
+    def getDisconnect(self):
+        if self.conn != None:
+            self.conn.close()
+            self.conn = None
+
+
+x = Connection("Localhost", "Bocadito", "3306", "root", "carlosftg1923")
+conn = x.getConnect()
+
+if conn != None:
+    print("conexion exitosa")
+else:
+    print("Error de conexion")
+# Forma 2 de conectar una base de datos
+# conexion = mysql.connector.connect(
+#     user="root",
+#     password="carlosftg1923",
+#     host="localhost",
+#     database = "Bocadito",
+#     port = "3306"
+# )
+# print(conexion)
