c2ddc416e86f4c734de8dd12ebc9d3062f092a31
Carlos Talavera <carlos.talavera16038@est.uca.edu.ni>
2024-03-16 20:27:33 -0600

    Se creo la tabla de clientes otra vez

--- DB/db.sql
+++ DB/db.sql
@@ -4,3 +4,14 @@ create database Bocadito;
 use Bocadito;
 -- Mostramos la base de datos
 show databases;
+-- Creamos la tabla de clientes
+create table clientes(
+	id int not null auto_increment,
+    primerNombre varchar(50),
+    segundoNombre varchar(50),
+    primerApellido varchar(50),
+    usuario varchar(20),
+    email varchar(50),
+    contraseña varchar(16),
+    primary key(id)
+);
